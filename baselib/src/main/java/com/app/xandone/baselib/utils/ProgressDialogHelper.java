af21450443a3d94a36bd007edb64030a22122f86
xandone <765478955@qq.com>
2022-04-20 16:04:07 +0800

    UI

--- baselib/src/main/java/com/app/xandone/baselib/utils/ProgressDialogHelper.java
+++ baselib/src/main/java/com/app/xandone/baselib/utils/ProgressDialogHelper.java
@@ -6,6 +6,8 @@ import android.view.WindowManager;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.app.xandone.baselib.R;
+import com.lxj.xpopup.XPopup;
+import com.lxj.xpopup.impl.LoadingPopupView;
 
 /**
  * author: Admin
@@ -14,16 +16,42 @@ import com.app.xandone.baselib.R;
  */
 public class ProgressDialogHelper {
 
+
+    private LoadingPopupView loadingPopup;
+
+    /**
+     * @deprecated
+     */
     private MaterialDialog dialog;
 
     private ProgressDialogHelper() {
     }
 
     public static ProgressDialogHelper getInstance() {
-        return Builder.instance;
+        return Builder.INSTANCE;
     }
 
     public void showLoading(Context context) {
+        if (loadingPopup != null && loadingPopup.dialog != null && loadingPopup.dialog.isShowing()) {
+            return;
+        }
+        loadingPopup = (LoadingPopupView) new XPopup.Builder(context)
+                .dismissOnBackPressed(false)
+                .dismissOnTouchOutside(false)
+                .isLightNavigationBar(true)
+                .isViewMode(false)
+                .asLoading("加载中")
+                .show();
+    }
+
+    /**
+     * @param context
+     * @deprecated
+     */
+    public void showLoading2(Context context) {
+        if (dialog != null && dialog.isShowing()) {
+            return;
+        }
         MaterialDialog.Builder builder = new MaterialDialog.Builder(context)
                 .content(R.string.progress_tip)
                 .titleColorRes(R.color.white)
@@ -46,13 +74,13 @@ public class ProgressDialogHelper {
     }
 
     public void dimissLoading() {
-        if (dialog != null) {
-            dialog.dismiss();
+        if (loadingPopup != null) {
+            loadingPopup.dismiss();
         }
     }
 
 
     private static class Builder {
-        private static ProgressDialogHelper instance = new ProgressDialogHelper();
+        private static final ProgressDialogHelper INSTANCE = new ProgressDialogHelper();
     }
 }
